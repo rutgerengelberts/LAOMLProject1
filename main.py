f79a7976995eae26753a617181d73dec77376a80
Rutger Engelberts <rutgere@kth.se>
2022-10-25 11:48:50 +0200

    read data and normalize

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 import pandas as pd
 
 # read data
-data = pd.read_csv('../LAOMLProject1/EastWestAirlinesCluster.csv')
+df = pd.read_csv('../LAOMLProject1/EastWestAirlinesCluster.csv')
+df = df.drop(columns=['ID#'])
+normalized_df = (df-df.min()) / (df.max()-df.min())
 
 # test
+
+print(normalized_df)
+print(normalized_df.columns)
