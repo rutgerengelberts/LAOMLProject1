dd6c4a870b7367fe5685c86f7bb7f872e3b4abad
Rutger Engelberts <rutgere@kth.se>
2022-10-26 13:17:26 +0200

    put kmeans into function and added plot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # packages
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # functions
@@ -16,7 +17,43 @@ def kernel_poly(x, y, r):
 def kernel_gauss(x, y, gamma):
     return np.exp(-gamma * np.linalg.norm(x - y) ** 2)
 
+def kmeans_euc(dat, K):
+    # initialise parameters
+    n, ncol = dat.shape
+    best = 10 ** 12 + 1
+    score = 10 ** 12
+    # initialise clusters and distances of points to clusters
+    C = np.clip(np.random.multivariate_normal(np.mean(data, axis=0), np.diag(np.std(data, axis=0)), size=(K,)), 0, 1)
+    dists = np.zeros(shape=(n, K))
+    # run until no improvement
+    iter = 0
+    while score < best:
+        # update new best
+        best = score
+        # update distances
+        for i in range(n):
+            for k in range(K):
+                dists[i, k] = kernel_euclidean(dat[i, :], C[k, :])
+        # assign to clusters based on distances
+        clusters = np.array([np.argmin(dists[i, :]) for i in range(n)])
+        # update centroid locations
+        for k in range(K):
+            # ixgrid = np.ix_(np.where(clusters == k)[0], np.arange(ncol))
+            # dat = data[ixgrid]
+            # C[k, :] = np.sum(data[ixgrid], axis=0)
+            C[k, :] = sum([dat[i, :] for i in range(n) if clusters[i] == k]) / np.count_nonzero(clusters == k)
+        # calc score
+        nclust = [np.count_nonzero(clusters == i) for i in range(K)]
+        score = sum([sum([dists[i, k] for i in range(n) if clusters[i] == k]) for k in range(K)])
+        # print division of clusters
+        # print(iter)
+        # print(nclust)
+        # print(clusters)
+        iter += 1
+    return score
 
+
+### MAIN
 # read data
 df = pd.read_csv('../LAOMLProject1/EastWestAirlinesCluster.csv')
 df = df.drop(columns=['ID#'])
@@ -25,72 +62,25 @@ data = normalized_df.to_numpy()
 
 
 # define parameters
-K = 4
-ncol = 11
-n = 3998
-
-
-
-### MAIN
-# initialise clusters
-#C = np.random.uniform(low=0, high=1, size=(K, ncol))
-C = np.clip(np.random.multivariate_normal(np.mean(data, axis=0), np.diag(np.std(data, axis=0)), size=(K,)), 0, 1)
-dists = np.zeros(shape=(n, K))
+nruns = 10
+Ks = [2, 3, 4, 5, 6]
+nKs = len(Ks)
 
 
 # run kmeans
-best = 10**12 + 1
-score = 10**12
-iter = 0
-while score < best:
-    # update new best
-    best = score
-    # update distances
-    for i in range(n):
-        for k in range(K):
-            dists[i, k] = kernel_euclidean(data[i, :], C[k, :])
-    # assign to clusters based on distances
-    clusters = np.array([np.argmin(dists[i, :]) for i in range(n)])
-    # update centroid locations
-    for k in range(K):
-        #ixgrid = np.ix_(np.where(clusters == k)[0], np.arange(ncol))
-        #dat = data[ixgrid]
-        #C[k, :] = np.sum(data[ixgrid], axis=0)
-        C[k, :] = sum([data[i, :] for i in range(n) if clusters[i] == k]) / np.count_nonzero(clusters == k)
-    # calc score
-    nclust = [np.count_nonzero(clusters == i) for i in range(K)]
-    score = sum([sum([dists[i, k] for i in range(n) if clusters[i] == k]) for k in range(K)])
-    # print division of clusters
-    print(iter)
-    print(nclust)
-    print(clusters)
-    iter += 1
+scores = np.zeros(shape=(nruns, nKs))
+for i in range(nKs):
+    ki = Ks[i]
+    print("k = "+str(ki)+" ...")
+    for ni in range(nruns):
+        scores[ni, i] = kmeans_euc(data, ki)
 
 
-# ones = np.ones(shape=(n,))
-# kernelmat = np.zeros(shape=(n, n))
-# for i in range(n):
-#     for j in range(i, n):
-#         kernelmat[i, j] = kernel_euclidean(data[i, :], data[j, :])
-#         kernelmat[j, i] = kernelmat[i, j]
-#
+# plot
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111)
+bp = ax.boxplot(scores)
+ax.set_xticklabels(['2', '3', '4'])
+plt.show()
 
-# #while clusters_new is not clusters
-# for z in range(2):
-#     # update distance matrix
-#     for k in range(K):
-#         sizeCk = np.count_nonzero(clusters == k)
-#         clustkerns = np.ix_(np.where(clusters == k), np.where(clusters == k))
-#         totsumclust = 1/sizeCk**2 * sum(kernelmat[clustkerns])
-#         for i in range(n):
-#             dists[i, k] = kernel_euclidean(data[i, :], data[i, :]) - 2/sizeCk * sum((kernelmat[i, j] for j in range(n)
-#                                                                                      if clusters[j] == k)) + totsumclust
-#     # update best cluster
-#     clusters = [np.argmin(dists[i, :]) for i in range(0, n)]
-
-
-# test
-print("\nFINAL RESULT:")
-print("\nitems per cluster " + str(nclust))
-print("\nscore " + str(score))
 
